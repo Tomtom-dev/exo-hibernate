5d7b76148e08d925c172aaf92b344dbd8a00b2b5
thomas Heddadji <thomasheddadji0@gmail.com>
2021-07-30 16:13:29 +0200

    test pour ajouter data

--- src/main/java/org/formhib/App.java
+++ src/main/java/org/formhib/App.java
@@ -1,8 +1,12 @@
 package org.formhib;
-
 import org.formhib.models.CategoryEntity;
 import org.formhib.repositories.CategoryRepository;
 import org.formhib.utils.SessionHibernate;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import java.sql.Timestamp;
 
 public class App {
     public static void main(String[] args) {
@@ -10,10 +14,17 @@ public class App {
         CategoryRepository categoryRepository =new CategoryRepository();
         CategoryEntity cat = new CategoryEntity();
         cat.setCode(5198465);
+
+        Date date=new Date();
+        SimpleDateFormat df2 = new SimpleDateFormat("yyyy-mm-dd");
+        String datetext = df2.format(date);
+
+        cat.setStamp(datetext);
+        cat.setId(1);
         cat.setLibelle("blamable87");
         categoryRepository.save(cat);
-
-
     }
+
+
 
 }
--- src/main/java/org/formhib/models/CategoryEntity.java
+++ src/main/java/org/formhib/models/CategoryEntity.java
@@ -1,7 +1,9 @@
 package org.formhib.models;
 
+import javax.persistence.*;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name ="categorie", schema = "materiel_location")
@@ -9,8 +11,31 @@ public class CategoryEntity {
     // code libelle
     private int code;
     private String libelle;
+    private Timestamp stamp;
+    private int id;
 
+    @Basic
+    @Column(name="stamp" , nullable=false)
+    public Timestamp getStamp() {
+        return stamp;
+    }
 
+    public void setStamp(Timestamp stamp) {
+        this.stamp = stamp;
+    }
+
+    @Id
+    @Column(name="id", nullable=false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name="code", nullable = false)
     public int getCode() {
         return code;
     }
@@ -19,6 +44,8 @@ public class CategoryEntity {
         this.code = code;
     }
 
+    @Basic
+    @Column(name = "libelle", nullable = false)
     public String getLibelle() {
         return libelle;
     }
